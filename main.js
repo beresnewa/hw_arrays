8f904dd2664e56917652bb832ed071cd87a2865c
beresnewa <ekaretinaberesneva5@gmail.com>
2020-12-31 14:30:59 +0300

    fix name of function

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
-
-
 const numbers = [2, 3, 5, 7, 11, 13, 17, 19];
 const newArr = [];
-const total = numbers.reduce((previous, current) => {
-    let sum = previous + current;
-    newArr.push(previous);
-    return sum;
-},);
-console.log(newArr);
+const currentSums = (array) =>{
+    array.reduce((previous, current) => {
+        let sum = previous + current;
+        newArr.push(previous);
+        return sum;
+    },);
+    console.log(newArr);
+} 
+currentSums(numbers);
